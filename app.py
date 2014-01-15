262aaa0f8781b30181e6b31d80ef2ffdd10f86be
markbreneman <mark.breneman@gmail.com>
2014-01-14 20:40:58 -0500

    Adding Clay to Voting

--- app.py
+++ app.py
@@ -89,8 +89,8 @@ if __name__ == "__main__":
 	app.debug = True
 	
 	port = int(os.environ.get('PORT', 5000)) # locally PORT 5000, Heroku will assign its own port
-	app.run(host='0.0.0.0', port=port)
-	# app.run(host='127.0.0.1', port=port)
+	# app.run(host='0.0.0.0', port=port)
+	app.run(host='127.0.0.1', port=port)
 
 
 
